6c97c0ee1ad4d241ec09d94ec3059c2061d68768
Jasmine Terry <jasmine.terry91@gmail.com>
2019-12-04 19:05:48 -0500

    menu complete

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -33,3 +33,35 @@ let menuItems = [
   Step 6: add the menu component to the DOM.
   
 */
+
+function menuComponent(arr){
+  const mainMenu = document.createElement('div');
+
+  const list = document.createElement('ul'); 
+  //create elemnts, div/ ul
+  
+  
+  mainMenu.classList.add('menu'); //create classes
+
+  mainMenu.appendChild(list); //append
+
+  arr.forEach(function(item){
+    const listItems = document.createElement('li');
+    listItems.textContent = item;
+    list.appendChild(listItems);
+  }) // arr method to add li's
+
+
+  const menuButton = document.querySelector('.menu-button'); //select 'menu-button class'
+
+  menuButton.addEventListener('click', event => {
+    mainMenu.classList.toggle('menu--open');
+  }) //click event, toggle 
+
+
+  return mainMenu; //return main menu
+} //create componenet funcion
+
+
+const nav = document.querySelector(".header"); 
+nav.appendChild(menuComponent(menuItems)); //add menu to dom
